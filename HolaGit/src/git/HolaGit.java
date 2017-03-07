68f667559167028bc6e10a5fac9689f1a2689564
Judit <juditochagavia@gmail.com>
2017-03-07 14:38:31 +0100

    Primer commit

---
+++ HolaGit/src/git/HolaGit.java
@@ -0,0 +1,10 @@
+package git;
+
+public class HolaGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Práctica Git para Entornos");
+	}
+
+}
